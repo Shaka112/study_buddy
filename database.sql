94e65edf9f0e09bc0c8a7d41a1434dcb7ebfbd87
Shaka112 <118933856+Shaka112@users.noreply.github.com>
2023-12-08 19:36:17 -0500

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,23 @@
+
+CREATE TABLE Users (
+userID VARCHAR(4),
+name VARCHAR(30),
+sex VARCHAR(10),
+age INTEGER,
+email VARCHAR(50),
+phone VARCHAR(15),
+PRIMARY KEY (userID));
+
+
+INSERT INTO Users VALUES('U201','Kewon Song','Male',36,'ksong@gmail.com','123-456-7890');
+INSERT INTO Users VALUES('U202','Frank Mancione','Male',14,'fmancione@hotmail.com','987-654-3210');
+INSERT INTO Users VALUES('U203','Angela Peterkin','Female',66,'angela@gmail.com','555-123-4567');
+INSERT INTO Users VALUES('U204','Stephanie Johnson','Female',16,'sjohnson@gmail.com','456-789-0123');
+INSERT INTO Users VALUES('U205','Rafael Bah','Male',17,'rbah@yahoo.com','789-012-3456');
+INSERT INTO Users VALUES('U206','Leisa Aras','Female',49,'laras@yahoo.com','234-567-8901');
+INSERT INTO Users VALUES('U207','Robert Salsberry','Male',55,'rsalsberry@hotmail.com','876-543-2109');
+INSERT INTO Users VALUES('U208','Alfina Chanti','Female',24,'achanti@hotmail.com','321-654-9870');
+INSERT INTO Users VALUES('U209','Connie Profaci','Female',32,'cprofaci@yahoo.com','654-987-0123');
+INSERT INTO Users VALUES('U210','Steven Plac','Male',17,'steven@yahoo.com','789-012-3456');
+INSERT INTO Users VALUES('U211','Cindy Lyon','Female',20,'cindy.lyon@hotmail.com','012-345-6789');
+INSERT INTO Users VALUES('U212','Nicole Gary','Female',26,'ngary@gmail.com','234-567-8901');
